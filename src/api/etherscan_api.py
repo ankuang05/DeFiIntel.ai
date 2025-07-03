9bd2b14cac02cfbea7058707c5bc0ef0321e3a1c
ankuang05 <andy3kuang@gmail.com>
2025-07-03 20:15:24 +0800

    Complete Phase 1: Project setup and API integrations

--- src/api/etherscan_api.py
+++ src/api/etherscan_api.py
@@ -4,7 +4,10 @@ from src.utils.config import CONFIG
 API_KEY = CONFIG['ETHERSCAN_API_KEY']
 BASE_URL = "https://api.etherscan.io/api"
 
-def get_token_info(token_address):
-    url = f"{BASE_URL}?module=token&action=tokeninfo&contractaddress={token_address}&apikey={API_KEY}"
+def get_token_transfers(token_address, address=None, startblock=0, endblock=99999999, sort="asc"):
+    # Fetch ERC20 token transfer events for a contract (token_address)
+    url = f"{BASE_URL}?module=account&action=tokentx&contractaddress={token_address}&startblock={startblock}&endblock={endblock}&sort={sort}&apikey={API_KEY}"
+    if address:
+        url += f"&address={address}"
     response = requests.get(url)
     return response.json()
--- test_api.py
+++ test_api.py
@@ -1,10 +1,10 @@
 from src.api.helius_api import get_wallet_transactions
 from src.api.coingecko_api import get_token_data_solana
-from src.api.etherscan_api import get_token_info
+from src.api.etherscan_api import get_token_transfers
 from src.api.rugdoc_api import check_token_rugdoc
 from src.api.twitter_api import search_tweets
 
-wallet = "YOUR_SOLANA_WALLET"
+wallet = "4F3qMHdHHuDzxkeX282WQmZRcsjv6ATUThLKDbDHaubj"
 token_solana = "So11111111111111111111111111111111111111112"
 token_eth = "0xdAC17F958D2ee523a2206206994597C13D831ec7"  # USDT
 
@@ -14,11 +14,11 @@ print(get_wallet_transactions(wallet)[:1])
 print("\n GeckoTerminal:")
 print(get_token_data_solana(token_solana))
 
-print("\n Etherscan:")
-print(get_token_info(token_eth))
+print("\n Etherscan (ERC20 transfers):")
+print(get_token_transfers(token_eth))
 
-print("\n RugDoc:")
-print(check_token_rugdoc(token_eth))
+# print("\n RugDoc:")
+# print(check_token_rugdoc(token_eth))
 
 print("\n Twitter (keyword = 'bonk'):")
 print(search_tweets("bonk"))
